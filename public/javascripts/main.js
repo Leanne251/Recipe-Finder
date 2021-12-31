3ad5a24d202a9b6c095ebd0207746e80bfe1da4d
Leanne251 <leannesmith251@gmail.com>
2021-12-31 12:03:51 +0000

    working project

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,6 @@
-
-
 const recipeForm = document.querySelector('#search-form');
 const searchRecipe = document.querySelector('#search-recipe');
-const recipeOptions = document.querySelector('.recipe-options');
-const searchButton = document.querySelector('#search-recipe');
+const recipeOptions = document.querySelector('.recipe-options')
 
 const findARecipe = document.querySelector('.find-a-recipe');
 findARecipe.addEventListener("click", searchIngredients)
@@ -11,92 +8,100 @@ findARecipe.addEventListener("click", searchIngredients)
 function searchIngredients () {
 
     recipeForm.style.display = "block";
-    searchButton.style.display = "block";
-
+    searchRecipe.style.display = "block";
+    
 }
 
-searchRecipe.addEventListener("click", getRecipeOptions)
+searchRecipe.addEventListener("click", getRecipeOptions)    
 
 async function getRecipeOptions (){
    
+    // must be three items as refers to three in put boxes
     let item1 = document.querySelector('#item1').value  
     let item2 = document.querySelector('#item2').value
     let item3 = document.querySelector('#item3').value
-
-        // const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${item1}&app_id=800e97dc&app_key=ee5cfbd4f93b8b69a5087c0f77ec80c7`);
     
         const response = await fetch (`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${item1},+${item2},+${item3}&number=3&apiKey=a555aea16fc3473197b4c2afa236c9f8`);
         console.log(response);
-        const data = await response.json();
+        const data = await response.json(); // array of data
         console.log(data)
 
-    // consider refactoring using a for loop? 
-    const option1 = document.querySelector(".option1")
-    const option2 = document.querySelector(".option2")
-    const option3 = document.querySelector(".option3")
+        recipeForm.style.display = "none";
+        searchRecipe.style.display = "none";
+
+        const recipeId = {}
+        const recipeArray = []
+
+        data.forEach((element, index) => {
+
+            const recipeButton = document.createElement('button') 
+           //  recipeButton.setAttribute("style", "background: blue; border: 1px solid black")
+            recipeButton.innerText = element.title
+            console.log("element.title", element.title )
+            
+            recipeOptions.appendChild(recipeButton)
+            console.log("element id", element.id )
+            recipeArray.push(element.id)
+            recipeId[element.title] = element.id
+            console.log("recipe ID Object", recipeId)
+            console.log("recipeArray ID's", recipeArray)
+
+})
+
+
+searchRecipe.addEventListener("click", getRecipeOptions)  
+
+const getRecipeInfo = (event) => accessApi(event, recipeId)
+
+recipeOptions.addEventListener("click", getRecipeInfo)
+
+ 
+async function accessApi(event, recipeId){
+
+
+    function findId (){
+        for (const key in recipeId) {
+            if (event.target.innerText === key){
+                return recipeId[key]
+            }    
+        }
+    }
     
-    option1.innerText = data[0].title;
-    option2.innerText = data[1].title;
-    option3.innerText = data[2].title;
+    const foundId = findId();
+    console.log(foundId);
 
-    const option1Id = data[0].id
-    console.log("option1", option1Id)
-    
-    const option2Id = data[1].id
-    const option3Id = data[2].id
-
-    recipeForm.style.display = "none";
-    searchButton.style.display = "none";
-    recipeOptions.style.display = "block";
-
-    async function event_handler(event, option1Id, option1) {
-   
-        
-   console.log("option1Id", option1Id)
-    const recipeInfo = await fetch (`https://api.spoonacular.com/recipes/${option1Id}/information?includeNutrition=false&apiKey=a555aea16fc3473197b4c2afa236c9f8`)
+    const recipeInfo = await fetch (`https://api.spoonacular.com/recipes/${foundId}/information?includeNutrition=false&apiKey=a555aea16fc3473197b4c2afa236c9f8`)
     const recipeData = await recipeInfo.json();
     console.log("recipe data", recipeData)
 
     const ingredients = [];
-
     (recipeData.extendedIngredients).forEach(element => {
         ingredients.push(element.name)
-    }  
-    )
+    })
     
     const ingredientsDiv = document.createElement('div')
-    ingredientsDiv.innerHTML = ingredients
+    ingredientsDiv.innerText = "What to Buy: \n" + ingredients + "\n";
 
     const method = document.createElement('div')
-    method.innerHTML = recipeData.instructions
+    method.innerHTML = `How To Cook \n ${recipeData.instructions}`
 
     const resultsContainer = document.querySelector('.results-flex-container')
 
-    resultsContainer.appendChild(option1);
+    const title = document.createElement('h4');
+    title.innerText = event.target.innerText;
+    resultsContainer.appendChild(title)
     const image = document.createElement('img');
     image.src = recipeData.image
     image.setAttribute("width", "300px")
     image.setAttribute("height", "300px")
     resultsContainer.appendChild(image)
+    // resultsContainer.innerText = "What To Buy: \n"
     resultsContainer.appendChild(ingredientsDiv)
+    // resultsContainer.innerText = "How To Cook: \n"
     resultsContainer.appendChild(method)
+    recipeOptions.style.display = "none";
 
-        
-      }
-
-   const showResults = (event) => event_handler(event, option1Id, option1 )
-
-    option1.addEventListener("click", showResults);
-    option2.addEventListener("click", showResults);
-    option3.addEventListener("click", showResults);
+}
 
 
-    }
-
-
-
-
-// }
-
-    
-
+}
